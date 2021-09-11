361743c72895877409aee42695b20d70d16de753
전승원 <78971821+louis7308@users.noreply.github.com>
2021-09-11 18:58:49 +0900

    err 코드 추가

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const dotenv = require('dotenv').config()
 const dbconfig = require('./config/database');
 const app = express();
 const http = require('http');
-//const { serialize } = require('v8');
-const hostname = '10.120.74.70';
-const port = 80;
+const port = 3000;
 
 const conn = mysql.createConnection(dbconfig);
 
@@ -30,7 +28,7 @@ app.post('/game_end', (req, res) => {
   const callGameEnd = 'CALL gameEnd(?, ?)';
   conn.query(callGameEnd, [user_id, user_score], (err, rows, field) => {
     if (err) {
-      res.status(500).send('데이터보내는 도중 서버 오류');
+      res.status(500).send('game_end 에서 데이터보내는 도중 서버 오류');
       console.log(err);
       console.log('에러는 query쪽에서 동작하다가 났어');
     }
@@ -46,7 +44,7 @@ app.get("/show_rank", (req, res) => {
   const query = 'SELECT u.device_id, u.nickname, r.nickname, r.score FROM user AS u LEFT JOIN `rank` AS r ON u.nickname = r.nickname WHERE score > 0 ORDER BY score DESC LIMIT 10';
   conn.query(query, (err, rows, fields) => {
     if (err) {
-      res.status(500).send('Internal Server Error');
+      res.status(500).send('show_rank 에서 서버 오류');
       console.log(err);
       console.log('Error while performing Query.');
     }
@@ -59,46 +57,11 @@ app.get("/show_rank", (req, res) => {
       var result_json = {};
       result_json["data"] = rows;
       console.log(result_json);
-      res.json(result_json["data"][0]);
+      res.json(result_json);
     }
   });
 });
 
-// app.post('/create_name', (req, res) => {
-//   const nickname = req.body.nickname;
-//   const device = req.body.device;
-//   const check_query = 'SELECT * FROM user WHERE nickname = ?'
-//   const insert_query = 'INSERT INTO user (device, nickname) VALUES(?, ?)'
-//   conn.query(check_query, nickname, (err, rows, field) => {
-//     console.log('create rows : ', rows);
-//     if (err) {
-//       res.status(500).send('Internal Server Error');
-//       console.log(err);
-//       console.log('Error while performing Query.');
-//     }
-//     else {
-//       let checkid = new Object();
-//       checkid.tf = false;
-
-//       if (rows.length == 0) {
-//         checkid.tf = false;
-//         console.log('checkid: ', chekcid);
-//         res.json(checkid);
-//       } else {
-//         conn.query()
-//         checkid.tf = true; // 중복됨 사용x
-//         res.json(checkid);
-//       }
-//     }
-//   })
-// })
-// insert into login_table lt
-// INNER JOIN user_info ui ON ui.some_id = lt.id
-// (ui.name, ui.address, lt.username, lt.password) 
-// values
-// ('John', 'wall street', 'john123', 'passw123')
-
-// INSERT INTO user lt INNER JOIN nickname ui ON ui.nickname
 
 
 app.post("/create_name", (req, res) => {
@@ -112,7 +75,7 @@ app.post("/create_name", (req, res) => {
   
   conn.query(nickCheckQuery, [user_name], (err, rows, fields) => {
     if (err) {
-      res.status(500).send('Internal Server Error');
+      res.status(500).send('create_name 에서 닉넴 중복 체크 하다가 오류 났습니다.');
       console.log(err);
       console.log('Error while performing Query.');
     }
@@ -120,14 +83,22 @@ app.post("/create_name", (req, res) => {
       if (rows.length == 0) {
         conn.query(insertQuery, [user_device, user_name], (err, rows, fields) => {
           if (err) {
-            res.status(500).send('Internal Server Error');
+            res.status(500).send('닉넴을 데이터베이스에 추가하다가 오류 났습니다');
             console.log(err);
             console.log('Error while performing Query.');
           }
           else {
             conn.query(ranknick_query, [user_name], (err, rows, fields) => {
-              console.log('test_ succeedd')
+              if(err) {
+                res.status(500).send('랭킹테이블에도 닉네임을 추가하다가 오류 났습니다.');
+                console.log(err)
+              }
+              console.log('ranking 닉넴 succeedd')
               conn.query(skilcharnick_query, [user_name], (err, rows, field) => {
+                if(err) {
+                  res.status(500).send('랭킹테이블에도 닉네임을 추가하다가 오류 났습니다.');
+                  console.log(err)
+                }
                 console.log('succeed skillcharactory');
               })
             })
@@ -153,7 +124,7 @@ app.post('/device', (req, res) => {
   const check_device = 'SELECT device_id FROM user WHERE device_id = ?'
   conn.query(check_device, device_id, (err, rows, field) => {
     if (err) {
-      res.status(500).send('서버 오류');
+      res.status(500).send('디바이스 검사 하다가 서버 오류');
     }
     else {
       let checkdevice = new Object();
@@ -170,29 +141,35 @@ app.post('/device', (req, res) => {
   })
 })
 
-app.post('/dataload', (req, res) => {
+app.get('/dataload', (req, res) => {
   const nickname = req.body.nickname;
+  const nicknamev2 = 'seung'
   console.log(req.body);
-  const dataload = 'SELECT * FROM skillcharactor WHERE nickname = ?'
-  conn.query(dataload, nickname, (err, rows, field) => {
+  const dataload = 'SELECT * FROM user left join skillcharactor ON user.nickname = skillcharactor.nickname WHERE user.nickname = ?'
+  conn.query(dataload, nicknamev2, (err, rows, field) => {
     console.log(rows);
     if (err) {
-      res.status(500).send('서버 오류 인뎁쇼??');
+      res.status(500).send('저장된 save 데이터를 읽는 도중 오류가 났습니다');
     }
     else {
       const data = {
         "nickname": rows[0].nickname,
-        "maxscore": rows[0].maxscore,
+        "highscore": rows[0].highscore,
         "coin": rows[0].coin,
         "character_level": {
-          "기사" : rows[0].기사
+          "기사" : rows[0].기사,
+          "법사" : rows[0].법사
         },
         "skill_level": {
           "돌진" : rows[0].돌진,
           "연속찌르기": rows[0].연속찌르기,
+          "검기날리기": rows[0].검기날리기,
           "회전공격": rows[0].회전공격,
-          "검이커져!": rows[0].검이커져,
-          "검기날리기": rows[0].검기날리기
+          "내려찍기": rows[0].내려찍기,
+          "밀쳐내기": rows[0].밀쳐내기,
+          "z자베기": rows[0].z자베기,
+          "흘리기": rows[0].흘리기,
+          "회복": rows[0].회복
         }
       }
       console.log('이게 data 값이다 ', rows[0]);
@@ -201,46 +178,42 @@ app.post('/dataload', (req, res) => {
   })
 })
 
-app.post('/datasave', (req, res) => {
+app.get('/datasave', (req, res) => {
   console.log(req.body);
-  const user = {
-    userData: '{"nickName":"seung","maxscore":0,"coin":100,"skill_level":{"돌진":1,"연속찌르기":1,"회전공격":1,"검이커져":1,"검기날리기":1}}'
-  }
-  const testuser = req.body.userData;
-  // console.log(testuser);
-
-  const obj = JSON.parse(testuser);
-  console.log(obj)
-  // const userdata = {
-  //   ""
+  // const user = {
+  //   userData: '{"nickname":"seung","highscore":2000,"coin":1020,"character_level":{"기사":1,"법사":0},"skill_level":{"돌진":1,"연속찌르기":1,"검기날리기":0,"회전공격":0,"내려찍기":0,"밀쳐내기":0,"z자베기":0,"흘리기":0,"회복":1}}'
   // }
+
+  const userdata = req.body.userData;
+  const obj = JSON.parse(userdata);
+  console.log(obj)
 
   const nickname = obj.nickname;
   const highscore = obj.highscore;
   const coin = obj.coin;
-  console.log(coin);
   const 기사 = obj.character_level.기사
   const 법사 = obj.character_level.법사
   const 돌진 = obj.character_level.돌진
   const 연속찌르기 = obj.skill_level.연속찌르기
   const 회전공격 = obj.skill_level.회전공격
-  const 검이커져 = obj.skill_level.검이커져
   const 검기날리기 = obj.skill_level.검기날리기
   const 삼단베기 = obj.skill_level.삼단베기
-  // console.log(obj);
-  // console.log(obj.nickName)
-
+  const 내려찍기 = obj.skill_level.내려찍기
+  const 밀쳐내기 = obj.skill_level.밀쳐내기
+  const 흘리기 = obj.skill_level.흘리기
+  const 회복 = obj.skill_level.회복
+  
   const save_user = 'UPDATE user SET nickname = ?, coin = ?, highscore = ? WHERE nickname = ?'
-  const save_skillcharactor = 'UPDATE skillcharactor SET 기사 = ?, 법사 = ?, 돌진 = ?, 연속찌르기 = ?, 회전공격 = ?, 검이커져 = ?, 검기날리기 = ?, 삼단베기 = ? WHERE nickname = ?'
+  const save_skillcharactor = 'UPDATE skillcharactor SET 기사 = ?, 법사 = ?, 돌진 = ?, 연속찌르기 = ?,검기날리기 = ?, 회전공격 = ?, 내려찍기 = ?, 밀쳐내기 = ?, 흘리기 = ?, 회복 = ? WHERE nickname = ?'
 
   conn.query(save_user, [nickname, coin, highscore, nickname], (err, rows, field) => {
-    if(err) {
-      res.status(500).send('서버오류 인뎁쇼잉');
+    if (err) {
+      res.status(500).send('user 테이블에 데이터를 저장 하는 도중 오류가 났습니다');
     }
     else {
-      conn.query(save_skillcharactor, [기사, 법사, 돌진, 연속찌르기, 회전공격, 검이커져, 검기날리기, 삼단베기], (err, rows, field) => {
-        if(err) {
-          res.status(500).send('두번째 구문 오류 안뎁쇼잉');
+      conn.query(save_skillcharactor, [기사, 법사, 돌진, 연속찌르기, 검기날리기, 회전공격, 내려찍기, 밀쳐내기, 흘리기, 회복, nickname], (err, rows, field) => {
+        if (err) {
+          res.status(500).send('skillcharactor 테이블에 데이터를 저장 하는 도중 오류가 났습니다');
         }
         else {
           res.send('데이터가 잘 저장 되었습니다.');
@@ -248,30 +221,9 @@ app.post('/datasave', (req, res) => {
       })
     }
   })
-  
-  // const testuser = user.substring(0,4);
-  // console.log(testuser);
-  // const test = req.body;
-  // const nickname = test.nickname
-  // const highscore = req.body.userData.highscore
-  // const coin = req.body.userData.coin
-  // const stonefront = req.body.userData.Skill_Level.돌진
-  // const faststing = req.body.userData.Skill_Level.연속찌르기
-  // const spinattack = req.body.userData.Skill_Level.회전공격
-  // const bigsword = req.body.userData.Skill_Level.검이커져
-  // const throwsword = req.body.userData.Skill_Level.검기날리기
-  // console.log(test);
-  // console.log(user);
-  // console.log(user.nickname);
-  // console.log(nickname);
-  // console.log(highscore);
-  // console.log(coin);
-  // console.log(bigsword);
-  // console.log(throwsword);
-  // console.log(faststing);
 
 })
 
-app.listen(port, hostname, () => {
+app.listen(port, () => {
   console.log('Server listening : 80');
 })
